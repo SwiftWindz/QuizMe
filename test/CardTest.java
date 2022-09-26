1875cac6316a796adbcff765c5c5345a149f8275
Phil Ganem <philipganem@gmail.com>
2022-09-26 11:47:29 -0400

    Tests init

---
+++ test/CardTest.java
@@ -0,0 +1,31 @@
+package test;
+import model.Card;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class CardTest {
+
+    @Test
+    /**
+     * Tests the constructor of Card
+     */
+    public void testCardInit(){
+        //Init 
+        Card card = new Card("Who is the best?", "Phil", 1);
+        //Analyze
+        String actualQuestion = card.getQuestion();
+        String actualAnswer = card.getAnswer();
+        int actualID = card.getID();
+        String expectedQuestion = "Who is the best?";
+        String expectedAnswer = "Phil";
+        int expectedID = 1;
+        //Assert
+        assertEquals(actualQuestion, expectedQuestion);
+        assertEquals(actualAnswer, expectedAnswer);
+        assertEquals(actualID, expectedID);
+
+    }
+    
+}
